25a81910022494a2ad24d3762b4252054a8d9a5f
Tushar Kant <nanu.clickity@gmail.com>
2019-09-02 12:24:32 +0530

    v3 setup

---
+++ config/webpack/loaders/js-loader.js
@@ -0,0 +1,15 @@
+const PATHS = require('../../paths')
+
+module.exports = function CreateJSLoader() {
+  const envOptions = {
+    cacheDirectory: process.env.NODE_ENV !== 'production',
+    compact: process.env.NODE_ENV === 'production'
+  }
+
+  return {
+    test: /\.(js|jsx)$/,
+    include: PATHS.SRC,
+    loader: require.resolve('babel-loader'),
+    options: envOptions
+  }
+}
