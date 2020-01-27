e0e52e00d471ccda5ab069d1854141f16050b6c9
Tushar Kant <nanu.clickity@gmail.com>
2020-01-28 04:28:39 +0530

    updates

---
+++ src/client/components/Sidebar/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import classnames from 'classnames'
+
+import s from './Sidebar.scss'
+
+export default function Sidebar({ className }) {
+  const cx = classnames(s.container, className)
+  return (
+    <div className={cx}>
+      <div className="sidebar-header">Sidebar Header</div>
+      <div className="sidebar-body">Sidebar Body</div>
+      <div className="sidebar-footer">Sidebar Footer</div>
+    </div>
+  )
+}
+
+Sidebar.propTypes = {
+  className: PropTypes.string
+}
